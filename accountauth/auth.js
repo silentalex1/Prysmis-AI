6ae4272a001809876d09274e6a2dc3da2b4f44aa
Zal <101911964+silentalex1@users.noreply.github.com>
2026-03-27 22:07:18 -0400

    Update auth.js

--- accountauth/auth.js
+++ accountauth/auth.js
@@ -7,19 +7,15 @@ const createBtn = document.getElementById('create-btn');
 userTab.addEventListener('click', () => {
     userSection.classList.remove('hidden');
     adminSection.classList.add('hidden');
-    userTab.classList.add('bg-white/5', 'text-blue-400');
-    userTab.classList.remove('text-gray-500');
-    adminTab.classList.remove('bg-white/5', 'text-blue-400');
-    adminTab.classList.add('text-gray-500');
+    userTab.classList.add('active');
+    adminTab.classList.remove('active');
 });
 
 adminTab.addEventListener('click', () => {
     adminSection.classList.remove('hidden');
     userSection.classList.add('hidden');
-    adminTab.classList.add('bg-white/5', 'text-blue-400');
-    adminTab.classList.remove('text-gray-500');
-    userTab.classList.remove('bg-white/5', 'text-blue-400');
-    userTab.classList.add('text-gray-500');
+    adminTab.classList.add('active');
+    userTab.classList.remove('active');
 });
 
 createBtn.addEventListener('click', () => {
